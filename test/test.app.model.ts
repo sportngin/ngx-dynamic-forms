1764cdfd66606a6125212a7906fa6ee73435e30b
Daniel Schaffer <daniel.schaffer@siplay.com>
2017-05-17 12:45:47 -0700

    fixed selection fields with prerequisites not being disabled while waiting for prerequisites

--- test/test.app.model.ts
+++ test/test.app.model.ts
@@ -3,21 +3,31 @@ import { Validators } from '@angular/forms';
 import { FormControlType }  from '../src/ts/form.control.type';
 import { Model }            from '../src/ts/model/model';
 
-export class TestAppModelSimpleFields extends Model {
+export class TestAppModelPage extends Model {
 
     constructor() {
         super(
             Model.textMember('textMember', Validators.required).addLabel('textMember'),
-        );
-    }
-
-}
-
-export class TestAppModelComplicatedFields extends Model {
-
-    constructor() {
-        super(
-            Model.member('datePicker', FormControlType.date).addLabel('datePicker')
+            Model.member('datePicker', FormControlType.date).addLabel('datePicker'),
+            Model.selectionMember('prerequisite')
+                .addLabel('prerequisite')
+                .addData('data', [
+                    { id: 1, name: 'option 1' },
+                    { id: 2, name: 'option 2' }
+                ])
+                .addData('itemLabel', 'name')
+                .addData('itemValue', 'id'),
+            Model.selectionMember('dependentField')
+                .addDependentControls('prerequisite')
+                .addLabel('dependentField')
+                .addData('data', (prerequisite: any) => {
+                    return [
+                        { id: 'a', name: `option ${prerequisite}-a`},
+                        { id: 'b', name: `option ${prerequisite}-b`}
+                    ]
+                })
+                .addData('itemLabel', 'name')
+                .addData('itemValue', 'id')
         )
     }
 
@@ -28,8 +38,8 @@ export class TestAppModel extends Model {
     constructor() {
         super(
             Model.pages(
-                Model.page('simple', new TestAppModelSimpleFields()),
-                Model.page('complicated', new TestAppModelComplicatedFields())
+                Model.page('page1', new TestAppModelPage()),
+                Model.page('page2', new TestAppModelPage())
             )
         )
     }
