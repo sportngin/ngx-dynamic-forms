f623f75598203e231eed4e427281a81b22e56de4
Daniel Schaffer <daniel.schaffer@siplay.com>
2017-09-19 11:55:05 -0700

    added missing exports

--- src/behavior/index.ts
+++ src/behavior/index.ts
@@ -3,13 +3,16 @@ export * from './behavior.service';
 export * from './behavior.type';
 export { behaviorProvider } from './behaviors';
 
+export { BackHandler }              from './back.behavior';
 export { DisplayValidationHandler } from './display.validation.behavior';
 export { EditItemHandler }          from './edit.item.behavior';
+export { ForwardHandler }           from './forward.behavior';
 export { IsDisabledHandler }        from './is.disabled.behavior';
 export { IsListItemControlRenderedHandler } from './is.list.item.control.rendered.behavior';
 export { IsRenderedHandler }        from './is.rendered.behavior';
 export { RemoveItemHandler }        from './remove.item.behavior';
 export { ResetItemHandler }         from './reset.item.behavior';
 export { SaveItemHandler }          from './save.item.behavior';
+export { SkipHandler }              from './skip.behavior';
 export { StateMessageDisplayHandler } from './state.message.display.behavior';
 export { SubmitHandler }            from './submit.behavior';
