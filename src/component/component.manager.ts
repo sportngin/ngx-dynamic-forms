bbe68a1ae4677707f07a9027c9fb7c27b2d5776c
Daniel Schaffer <daniel.schaffer@siplay.com>
2017-09-25 16:45:34 -0700

    removed stray console.log

--- src/component/component.manager.ts
+++ src/component/component.manager.ts
@@ -25,7 +25,6 @@ export class ComponentManager {
     private mergeOptionsConfig<TModelElement extends ModelElement>(element: TModelElement): TModelElement {
         if (!element['__mergedConfig']) {
             let defaultOptions = map(element.optionsConfigKeys, key => this.config.defaultOptions[key]);
-            console.log(`defaultOptions ${element.elementType}`, defaultOptions);
             if (defaultOptions) {
                 mergeWith(element, ...defaultOptions, element, optionsMerge);
             }
