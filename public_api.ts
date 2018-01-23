e1144373dc4c709c51dcd59621548c506b0510ae
Daniel Schaffer <daniel.schaffer@siplay.com>
2018-01-23 14:27:30 -0800

    build fixes

--- public_api.ts
+++ public_api.ts
@@ -1,5 +1,11 @@
-/**
- * @module
- * @description Entry point for all public APIs
- */
-export * from './src/public_api';
+export * from './src/behavior/index';
+export * from './src/component/index';
+export * from './src/component/element/index';
+export * from './src/component/member/index';
+export * from './src/config/index';
+export * from './src/model/index';
+export * from './src/model/element/index';
+export * from './src/model/member/index';
+export * from './src/util/index';
+
+export { DynamicFormsModule } from './src/dynamic.forms.module';
